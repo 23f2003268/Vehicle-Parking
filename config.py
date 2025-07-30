651e92c4a64d8e1f89a419ee738236557298b00a
23f2003268 <23f2003268@ds.study.iitm.ac.in>
2025-07-31 01:56:35 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///vehicle_parking.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
